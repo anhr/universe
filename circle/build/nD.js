b7622f7e1a941c36f772ae659109282f4a51280a
anhr <anhr@mail.ru>
2024-05-11 16:09:01 +0700

    circle\nd
    >npm run build

---
+++ circle/build/nD.js
@@ -0,0 +1,50 @@
+/**
+ * @module ND
+ * @description N-dimensional graphics.
+ *
+ * @author [Andrej Hristoliubov]{@link https://anhr.github.io/AboutMe/}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.ND = {})));
+}(this, (function (exports) { 'use strict';
+
+/**
+ * @module ND
+ * @description N-dimensional graphics
+ * @author [Andrej Hristoliubov]{@link https://github.com/anhr}
+ *
+ * @copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * @license under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @see [4D Geometry Viewer]{@link https://github.com/anhr/humke-4d-geometry}
+ * @see [Tesseract]{@link https://ciechanow.ski/tesseract/}
+ * @see [4D-Shapes]{@link https://artemonigiri.github.io/4D-Shapes/}
+ * @see [The Regular Polychora]{@link https://www.qfbox.info/4d/regular}
+*/
+class ND {
+	constructor(n, settings) {}
+	get defaultColor() {
+		return 'lime';
+	}
+}
+
+exports['default'] = ND;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=nD.js.map
---
+++ circle/build/nD.module.js
@@ -0,0 +1,40 @@
+/**
+ * @module ND
+ * @description N-dimensional graphics.
+ *
+ * @author [Andrej Hristoliubov]{@link https://anhr.github.io/AboutMe/}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @module ND
+ * @description N-dimensional graphics
+ * @author [Andrej Hristoliubov]{@link https://github.com/anhr}
+ *
+ * @copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * @license under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @see [4D Geometry Viewer]{@link https://github.com/anhr/humke-4d-geometry}
+ * @see [Tesseract]{@link https://ciechanow.ski/tesseract/}
+ * @see [4D-Shapes]{@link https://artemonigiri.github.io/4D-Shapes/}
+ * @see [The Regular Polychora]{@link https://www.qfbox.info/4d/regular}
+*/
+class ND {
+	constructor(n, settings) {}
+	get defaultColor() {
+		return 'lime';
+	}
+}
+
+export default ND;
+//# sourceMappingURL=nD.module.js.map
