c4cbdcf0b1430bc36649090aa1628e6873342449
anhr <anhr@mail.ru>
2024-05-13 06:21:54 +0700

    build circle

--- circle/circularUniverse.js
+++ circle/circularUniverse.js
@@ -1,6 +1,8 @@
-/**
- * @module ND
- * @description N-dimensional graphics
+﻿/**
+ * @module CircularUniverse
+ * @description All the vertices of the Universe form a circle.
+ * All the vertices of the CircularUniverse form a circle.
+ *
  * @author [Andrej Hristoliubov]{@link https://github.com/anhr}
  *
  * @copyright 2011 Data Arts Team, Google Creative Lab
@@ -10,18 +12,55 @@
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
 */
 
 import Universe from '../universe.js';
 
-class CircularUniverse {
+//const sCircularUniverse = 'CircularUniverse';
 
-	constructor() {
+class CircularUniverse extends Universe {
 
+	/**
+	 * CircularUniverse.
+	 * All the vertices of the CircularUniverse form a circle.
+	 * @param {object} [universeSettings] <b>CircularUniverse</b> class settings. See <a href="./module-Universe-Universe.html" target="_blank">Universe classSettings</a>.
+	 **/
+	constructor(universeSettings = {}, myThreeOptions = {}) {
+
+		super(universeSettings, myThreeOptions);
+
+	}
+	getHuperSphere(scene, options, universeSettings) {
+
+		return new Circle(options, universeSettings);
+
+	}
+	name(getLanguageCode) {
+
+		//Localization
+
+		const lang = {
+
+			name: 'Circular Universe',
+
+		};
+
+		switch (getLanguageCode()) {
+
+			case 'ru'://Russian language
+
+				lang.name = 'Круговая вселенная';
+
+				break;
+
+		}
+		return lang.name;
 
 	}
 
 }
-export default CircularUniverse;
 
+import Circle from '../../../commonNodeJS/master/HuperSphere/circle.js'
+CircularUniverse.ND = Circle.ND;
+CircularUniverse.edgesCreationMethod = Circle.edgesCreationMethod;
+export default CircularUniverse;
--- universe.js
+++ universe.js
@@ -13,7 +13,7 @@
  * http://www.apache.org/licenses/LICENSE-2.0
 */
 
-//import * as THREE from '../../three.js/dev/src/Three.js';
+import * as THREE from '../../three.js/dev/src/Three.js';
 //import * as THREE from '../../three.js/dev/build/three.module.js';
 //import * as THREE from 'https://threejs.org/build/three.module.js';
 //import * as THREE from 'https://raw.githack.com/anhr/three.js/dev/build/three.module.js';
@@ -33,10 +33,128 @@ class Universe// extends HuperSphere
 
 	constructor(universeSettings = {}, myThreeOptions = {}) {
 
-		
+		myThreeOptions.scales ||= {};
+		myThreeOptions.scales.x ||= {};
+		myThreeOptions.scales.y ||= {};
+		myThreeOptions.scales.text ||= {};
+		myThreeOptions.scales.text.precision = myThreeOptions.scales.text.precision != undefined ? myThreeOptions.scales.text.precision : 1;
+		myThreeOptions.scales.text.rect ||= {};
+		myThreeOptions.scales.text.rect.displayRect = myThreeOptions.scales.text.rect.displayRect != undefined ? myThreeOptions.scales.text.rect.displayRect : false;
+
+		myThreeOptions.orbitControls ||= {};
+		myThreeOptions.orbitControls.enableRotate = myThreeOptions.orbitControls.enableRotate != undefined ? myThreeOptions.orbitControls.enableRotate : false;
+
+		myThreeOptions.camera ||= {};
+		if (myThreeOptions.camera.position) {
+
+			if (myThreeOptions.camera.position instanceof Array === true)
+				myThreeOptions.camera.position = new THREE.Vector3(myThreeOptions.camera.position[0], myThreeOptions.camera.position[1], myThreeOptions.camera.position[2]);
+		}
+		else myThreeOptions.camera.position = new THREE.Vector3(0, 0, 2);
+
+		//		myThreeOptions.stereoEffect = myThreeOptions.stereoEffect != undefined ? myThreeOptions.stereoEffect : false;
+
+		myThreeOptions.canvas ||= {};
+		myThreeOptions.canvas.noButtonFullScreen = myThreeOptions.canvas.noButtonFullScreen != undefined ? myThreeOptions.canvas.noButtonFullScreen : true;
+
+		new MyThree((scene, options) => {
+
+			universeSettings.projectParams ||= {};
+			universeSettings.projectParams.scene = scene;
+
+			universeSettings.settings ||= {};
+			universeSettings.settings.object ||= {};
+			universeSettings.settings.object.name ||= this.name(options.getLanguageCode);
+
+			this.huperSphere = this.getHuperSphere(scene, options, universeSettings);
+			this.huperSphere.child = this;
+
+		}, myThreeOptions/*{
+
+			//axesHelper: false,
+			orbitControls: { enableRotate: false, },
+			dat: { guiSelectPoint: { point: (options, dat, fMesh) => { return new HuperSphere.ND.gui(options, dat, fMesh); }, }, },
+			//dat: false,
+			camera: { position: new THREE.Vector3( 0, 0, 2 ) },
+			stereoEffect: false,
+			//canvasMenu: false,
+			scales: {
+
+				x: {},
+				y: {},
+				z: undefined,//{},
+				//posAxesIntersection: new THREE.Vector3( -1, -1, 0 ),
+				text: { precision: 1, rect: { displayRect: false, }, }
+
+			},
+			canvas: {
+
+				noButtonFullScreen: true,
+
+			},
+			//point: { size: 0.0 },
+			playerOptions: {
+				
+				max: Infinity,
+				interval: 100,
+				
+			}
+			
+		}*/);
+
+	}
+	name(options) {
+
+		//Localization
+
+		const lang = {
+
+			name: 'Universe',
+
+		};
+
+		switch (options.getLanguageCode()) {
+
+			case 'ru'://Russian language
+
+				lang.name = '¬селенна€';
+
+				break;
+
+		}
+		return lang.name;
+
+	}
+	arc(aAngleControls, lang, arcAngles) {
+
+		const arcEdges = [];
+		for (let i = 0; i < (aAngleControls.MAX_POINTS - 1); i++) arcEdges.push([i, i + 1]);
+		aAngleControls.arc = this.huperSphere.line({
+
+			cookieName: 'arc',//если не задать cookieName, то настройки дуги будут братьс€ из настроек вселенной
+			//edges: false,
+			object: {
+
+				name: lang.arc,
+				geometry: {
+
+					MAX_POINTS: aAngleControls.MAX_POINTS,
+					angles: arcAngles,
+					//opacity: 0.3,
+					indices: {
+
+						edges: arcEdges,
+
+					}
+
+				}
+
+			},
+
+		});
+
 	}
 
 }
 
 export default Universe;
-
