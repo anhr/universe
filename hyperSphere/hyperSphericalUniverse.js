9e9423c6281874eb71008b1da476f431aa60d4b7
anhr <anhr@mail.ru>
2026-05-31 07:08:24 +0700

    jsdoc

--- hyperSphere/hyperSphericalUniverse.js
+++ hyperSphere/hyperSphericalUniverse.js
@@ -33,6 +33,44 @@ class HypersphericalUniverse extends SphericalUniverse {
 	/**
 	 * All the vertices of the <b>HypersphericalUniverse</b> form a 3 dimensional [hypersphere]{@link https://en.wikipedia.org/wiki/N-sphere}.
 	 * @param {object} [classSettings={}] See <a href="../../../commonNodeJS/master/HyperSphere/jsdoc/module-HyperSphere-HyperSphere.html" target="_blank">HyperSphere classSettings</a> parameter.
+	 * <pre>
+	 * Additional settings:
+	 * </pre>
+	 * @param {object} [compute={}] Compute methods
+	 * @param {boolean} [compute.isUseCPU] Compute methods
+			compute: {
+				//isUseCPU: true,
+				config: {
+//					type: 'START_COMPUTE',//Default 'START_COMPUTE'
+
+					//Select one:
+					
+					RANDOM_POINTS: 0,//Случайная точка не вычисляется. Вместо этого возвращается PSEUDO_RANDOM.
+					
+			        //Вычисляется случайное число.
+			        //В GPU для получения случайного числа применяется хеширование(Hashing).Простой генератор псевдослучайных чисел(PCG).Permuted Congruential Generator(Перемешанный конгруэнтный генератор).
+			        //Этот метод лучше всего вычисляет случайное число, но требует много времени на вычисления если не оптимизировать Google Chrome.
+			        //Инструкция по оптимизации находится в Technical Guide: Enabling High-Performance GPU for Google Chrome https://github.com/anhr/universe/blob/main/hyperSphere/HUniverseEngine.md#technical-guide-enabling-high-performance-gpu-for-google-chrome Инструкцию по оттимизации смотри в D:\My documents\MyProjects\webgl\three.js\GitHub\universe\main\hyperSphere\webGPUHUniverse.js
+					//Значение по умолчанию
+			        //RANDOM_POINTS = 1;
+					
+					//PSEUDO_RANDOM: 0.1,//Have effect only if RANDOM_POINTS: 0. Default 0.5
+					
+					//DAMPING: 0.95,//Демпфирование движения. Default 0.95
+
+					//Сила отталкивания. Чем меньше значение, тем слабее силы отталкивания между точками, и тем медленнее они двигаются
+					//REPULSION_STRENGTH: 10,//По умолчанию не определено и зависит от количества вершин REPULSION_STRENGTH = config.a / classSettings.settings.object.geometry.angles.length.
+					//a: 50,//имеет эффект только если не определен REPULSION_STRENGTH. Default 50
+					
+
+					//Hyperbola parametr. See RandomVertice.calculateHyperbola
+					//p: 0 Прямая линия: y = x (через точки (0,0) и (π,π)).
+					//p: 1 Два отрезка: вертикальный и горизонтальный
+					//0 < p < 1 Гипербола. График гиперболы млжно посмотреть на http://localhost/anhr/commonNodeJS/master/HyperSphere/Examples/hyperbola.html
+					p: 0.99,//Default 0
+
+					//LOG: true,//log to console all calculated vertices. Default undefined
+				},
 	 **/
 	constructor(classSettings = {}, myThreeOptions) { super(classSettings, myThreeOptions); }
 	getHyperSphere(options, classSettings) {
